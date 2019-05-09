6c8f25e76f14edb85528a33c6f74bfe7756056db
akkuratist <36897138+akkuratist@users.noreply.github.com>
2019-05-09 16:23:14 +0300

    Исправления по PR

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,22 @@
 var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
+var menuToggle = document.querySelector('.page-header__menu-toggle');
 
+menuToggle.classList.remove('page-header__menu-toggle--nojs');
+menuToggle.classList.add('page-header__menu-toggle--closed');
 navMain.classList.remove('main-nav--nojs');
 navMain.classList.add('main-nav--closed');
 
-navToggle.addEventListener('click', function () {
+menuToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.remove('main-nav--closed');
     navMain.classList.add('main-nav--opened');
+    menuToggle.classList.remove('page-header__menu-toggle--closed');
+    menuToggle.classList.add('page-header__menu-toggle--opened');
+
   } else {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
+    menuToggle.classList.add('page-header__menu-toggle--closed');
+    menuToggle.classList.remove('page-header__menu-toggle--opened');
   }
 });
